e0d62d8eb9624bd5b2a8c1f0994f1249c81c9c34
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-08-08 00:58:18 +0900

    修正

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -10,17 +10,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
     Route::post('/logout', [LoginController::class, 'logout']);
+});
 
-    // post
-    Route::group(['prefix' => 'posts'], function () {
-        Route::get('/', [PostController::class, 'index']);
-        Route::post('/', [PostController::class, 'store']);
-        Route::get('/{post}', [PostController::class, 'show']);
-        Route::patch('/{post}', [PostController::class, 'update']);
-        Route::delete('/{post}', [PostController::class, 'destroy']);
-    });
+// post
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::post('/', [PostController::class, 'store']);
+    Route::get('/{post}', [PostController::class, 'show']);
+    Route::patch('/{post}', [PostController::class, 'update']);
+    Route::delete('/{post}', [PostController::class, 'destroy']);
 });
 
 // ユーザー登録
