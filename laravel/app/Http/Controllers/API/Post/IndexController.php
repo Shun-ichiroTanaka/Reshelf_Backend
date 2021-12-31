f2be5a11f2510c8da259610b6fe70986152d8db2
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-12-31 23:29:10 +0900

    投稿にいいねとタグ情報も返す

--- laravel/app/Http/Controllers/API/Post/IndexController.php
+++ laravel/app/Http/Controllers/API/Post/IndexController.php
@@ -5,16 +5,15 @@ namespace App\Http\Controllers\Api\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-
 class IndexController extends Controller
 {
-
     /**
-     * Display a listing of the resource.
+     * 本の情報とそれに付随する情報も渡す
      *
      * @return \Illuminate\Http\Response
      */
     public function __invoke(){
-        return new PostResource(Post::all());
+        return new PostResource(Post::all()->sortByDesc('created_at')
+        ->load(['user', 'likes', 'tags']));
     }
 }
