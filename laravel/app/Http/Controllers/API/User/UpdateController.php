624060104f26e26a1136bf18b7011b178757bd79
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2022-01-16 02:15:19 +0900

    ユーザーアップデート

--- laravel/app/Http/Controllers/API/User/UpdateController.php
+++ laravel/app/Http/Controllers/API/User/UpdateController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers\Api\User;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\User as UserResource;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\User as UserResource;
 
 class UpdateController extends Controller
 {
@@ -15,22 +14,10 @@ class UpdateController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request, User $user)
+    public function __invoke(Request $request)
     {
-        //set validation
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-        ]);
-
-        //response error validation
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        //update to database
-        $user->update([
-            'name' => $request->name,
-        ]);
+        $user=Auth::user();
+        $user->fill($request->all())->save();
 
         return new UserResource($user);
     }
