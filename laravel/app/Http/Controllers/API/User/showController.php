4a36397ebd9b3d34d3e84f39ae12fab6c608e95b
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2022-01-19 23:27:35 +0900

    一旦戻す

--- laravel/app/Http/Controllers/API/User/showController.php
+++ laravel/app/Http/Controllers/API/User/showController.php
@@ -2,26 +2,14 @@
 
 namespace App\Http\Controllers\Api\User;
 
-use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\User as UserResource;
 
 class ShowController extends Controller
 {
-    public function __invoke(string $name)
+    public function __invoke(Request $request)
     {
-
-        $user = User::where('name', $name)->first()
-        ->load([
-            'post.user',
-            'post.likes',
-            // 'post.tags'
-        ]);
-
-        $post = $user->post->sortByDesc('created_at');
-
-        return response()->json([
-            'user' => $user,
-            'post' => $post,
-        ]);
+        return new UserResource($request->user());
     }
 }
