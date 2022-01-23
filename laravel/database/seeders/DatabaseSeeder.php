2d8e76afdbe06312002833c3c0e97ce92200cb4a
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2022-01-23 17:26:02 +0900

    seedの修正

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         User::create([
             'name' => 'aaa',
             'email' => 'aaa@aaa.com',
             'password' => bcrypt('password'),
+        ]);
+
+        User::create([
+            'name' => 'bbb',
+            'email' => 'bbb@bbb.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::create([
+            'name' => 'ccc',
+            'email' => 'ccc@ccc.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::create([
+            'name' => 'ddd',
+            'email' => 'ddd@ddd.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Post::create([
+            'title' => 'sample',
+            'price' => '2500',
+            'body' => '本文です。',
+            'describe' => 'コースの説明が入ります。',
+            'user_id' => '1',
+        ]);
+
+        Post::create([
+            'title' => 'sample',
+            'price' => '500',
+            'body' => '本文です。',
+            'describe' => 'コースの説明が入ります。',
+            'user_id' => '2',
+        ]);
+
+        Post::create([
+            'title' => 'sample',
+            'price' => '5000',
+            'body' => '本文です。',
+            'describe' => 'コースの説明が入ります。',
+            'user_id' => '3',
+        ]);
+
+        Post::create([
+            'title' => 'sample',
+            'price' => '10000',
+            'body' => '本文です。',
+            'describe' => 'コースの説明が入ります。',
+            'user_id' => '4',
         ]);
     }
 }
