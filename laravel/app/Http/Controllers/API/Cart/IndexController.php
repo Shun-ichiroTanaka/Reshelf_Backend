6474d06911339107561c37f0882d87ef2bbeafda
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2022-01-23 20:28:20 +0900

    カート index controller修正

--- laravel/app/Http/Controllers/API/Cart/IndexController.php
+++ laravel/app/Http/Controllers/API/Cart/IndexController.php
@@ -2,29 +2,25 @@
 
 namespace App\Http\Controllers\Api\Cart;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Cart as CartResource;
 
 class IndexController extends Controller
 {
+    // カート商品と合計金額を返す
     public function __invoke()
     {
-        // dd('テスト');
         $user = User::findOrFail(Auth::id());
         $posts = $user->posts;
-        $cart_counts = 0;
         $totalPrice = 0;
 
         foreach($posts as $post){
-            $cart_counts += $post->carts->count();
             $totalPrice += $post->price;
         }
-        // // dd($posts, $cart_counts);
 
-        // // return view('user.cart',
-        // //     compact('posts', 'cart_counts'));
-        return new CartResource($user,$posts, $cart_counts, $totalPrice);
+        return new CartResource($posts, $totalPrice);
     }
 }
