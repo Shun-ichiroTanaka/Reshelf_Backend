39b780306dba9a0e1c2c6d6c9cbffa9bbbb1f5de
Kuroro <shun.ichiro.tanaka16@gmail.com>
2022-02-06 20:32:25 +0900

    ユーザーresponse修正

--- laravel/app/Http/Resources/User.php
+++ laravel/app/Http/Resources/User.php
@@ -18,6 +18,13 @@ class User extends JsonResource {
 			'id' => $this->id,
 			'name' => $this->name,
 			'email' => $this->email,
+            'description' => $this->description,
+            'icon_img' => $this->icon_img,
+            'cover_img' => $this->cover_img,
+            'twitter_url' => $this->twitter_url,
+            'youtube_url' => $this->youtube_url,
+            'facebook_url' => $this->facebook_url,
+            'linked_in_url' => $this->linked_in_url,
 			'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'posts' => PostResource::collection($this->posts)
